54b0359b1b5f87e1ee0da992207edf474c5de3e6
cdgutierrezd <cdgutierrezd@ufpso.edu.co>
2026-05-11 15:53:51 -0500

    Borrados

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,9 +3,10 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
-use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
-use Symfony\Component\HttpKernel\Exception\HttpException;
+use App\Exceptions\NotFoundHttpException;
+use App\Exceptions\ForbiddenHttpException;
+use App\Exceptions\InternalServerErrorException;
+use App\Exceptions\SessionExpiredException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -29,29 +30,20 @@ class Handler extends ExceptionHandler
 
     public function render($request, Throwable $exception)
     {
-        // Error 404 - Página no encontrada
         if ($exception instanceof NotFoundHttpException) {
             return response()->view('errors.404', [], 404);
         }
 
-        // Error 403 - Acceso denegado
-        if ($exception instanceof AccessDeniedHttpException) {
+        if ($exception instanceof ForbiddenHttpException) {
             return response()->view('errors.403', [], 403);
         }
 
-        // Error 500 - Error del servidor
-        if ($exception instanceof HttpException && $exception->getStatusCode() == 500) {
+        if ($exception instanceof InternalServerErrorException) {
             return response()->view('errors.500', [], 500);
         }
 
-        // Error 419 - Sesión expirada (CSRF)
-        if ($exception instanceof \Illuminate\Session\TokenMismatchException) {
+        if ($exception instanceof SessionExpiredException) {
             return response()->view('errors.419', [], 419);
-        }
-
-        // Error 503 - Servicio no disponible
-        if ($exception instanceof HttpException && $exception->getStatusCode() == 503) {
-            return response()->view('errors.503', [], 503);
         }
 
         return parent::render($request, $exception);
