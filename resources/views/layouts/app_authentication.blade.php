1acd7ac0d6cec05fab494b4e3c73388198abfc65
cdgutierrezd <cdgutierrezd@ufpso.edu.co>
2026-06-04 11:07:22 -0500

    prueba

--- resources/views/layouts/app_authentication.blade.php
+++ resources/views/layouts/app_authentication.blade.php
@@ -1,39 +1,9 @@
 <!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html>
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="icon" href="favicon.ico" type="image/x-icon">
-
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>{{ config('app.name', 'Laravel') }}</title>
-
-    <!-- Google Font: Source Sans Pro -->
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
-    <!-- Font Awesome -->
-    <link rel="stylesheet" href="{{asset('backend/plugins/fontawesome-free/css/all.min.css')}}">
-    <!-- icheck bootstrap -->
-    <link rel="stylesheet" href="{{asset('backend/plugins/icheck-bootstrap/icheck-bootstrap.min.css')}}">
-    <!-- Theme style -->
-    <!--
-    <link rel="stylesheet" href="{{asset('backend/dist/css/adminlte.css')}}">
-    -->
-    <link rel="icon" href="favicon.ico" type="image/x-icon">
-    <!-- Scripts -->
-    <!--@vite(['resources/sass/app.scss', 'resources/js/app.js'])-->
-
-    @stack('css')
+    <title>Test</title>
 </head>
-<body class="hold-transition login-page">
+<body>
     @yield('content')
 </body>
-    <!-- jQuery -->
-    <script src="{{asset('backend/plugins/jquery/jquery.min.js')}}"></script>
-    <!-- Bootstrap 4 -->
-    <script src="{{asset('backend/plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
-    <!-- AdminLTE App -->
-    <script src="{{asset('backend/dist/js/adminlte.min.js')}}"></script>
-    @stack('scripts')
 </html>
